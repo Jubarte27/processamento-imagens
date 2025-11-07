b9ca7be7041c69879854eddf5f1190e507a74e70
Matheus Machado <matheusmazar@gmail.com>
2025-11-07 18:29:12 -0300

    lerp com hack

--- q1/imgs_in_docked_figures.m
+++ q1/imgs_in_docked_figures.m
@@ -1,7 +1,19 @@
-function out = imgs_in_docked_figures(imgs, names, varargin)
-    out = gobjects(1, length(imgs));
+function imgs_in_docked_figures(imgs, img_names, names, varargin)
+
+    if ~usejava('desktop')
+        mkdir('.out');
+        for i = 1:length(imgs)
+            base_image_name = strrep(strrep(strrep(img_names{i}, '.jpg', ''), '.png', ''), '.tif', '');
+            name = strrep(lower(names{i}), ' ', '_');
+
+            imwrite(imgs{i}, strcat('.out/', base_image_name, '_', name, '.png'));
+        end
+        return
+    end
+
+
     for i = 1:length(imgs)
-        out(i) = figure( ...
+        figure( ...
             'WindowStyle', 'docked', ...
             'NumberTitle', 'off', ...
             'Units', 'normalized', ...
--- q1/lerp.m
+++ q1/lerp.m
@@ -1,24 +1,39 @@
 function out = lerp(mat, scale, dim)
     s = size(mat);
-    new_s = size(mat);
-    new_s(dim) = round(s(dim) * scale);
-    out = zeros(new_s, 'like', mat);
+    n_dim = ndims(mat);
 
-    clip = @(x) max(min(x, s(dim)), 1);
+    new_s = s;
+    new_s(dim) = max(1, round(s(dim) * scale));
 
-    for i = 1:new_s(dim)
-        i_in_old_scale = (i - 0.5) / scale + 0.5; %pixel center
+    % computar indices
+    i_new = (1:new_s(dim))';
+    i_in_old = (i_new - 0.5) / scale + 0.5; % centro do pixel ao invés da borda
 
-        a = i_in_old_scale - floor(i_in_old_scale);
+    Q1 = floor(i_in_old);
+    Q2 = Q1 + 1;
+    a  = i_in_old - Q1;
 
-        Q1 = floor(i_in_old_scale);
-        Q2 = ceil(i_in_old_scale);
+    Q1 = max(min(Q1, s(dim)), 1);
+    Q2 = max(min(Q2, s(dim)), 1);
 
-        Q1 = clip(Q1);
-        Q2 = clip(Q2);
+    a(Q1 == Q2) = 0;
 
-        interpolated = (1 - a) * get_at_dim(mat, dim, Q1) + a * get_at_dim(mat, dim, Q2);
+    % hack de colocar a dimensão na primeira coluna e colapsar o resto
+    order = [dim, 1:dim-1, dim+1:n_dim];
+    mat_p = permute(mat, order);
+    s_mat_p = size(mat_p);
+    mat2D = reshape(mat_p, s_mat_p(1), []);
 
-        out = set_at_dim(out, dim, i, interpolated);
-    end
+    vals_Q1 = mat2D(Q1, :);
+    vals_Q2 = mat2D(Q2, :);
+
+    a = repmat(a, [1, size(vals_Q1, 2)]);
+
+    % interpolacao
+    out2D = (1 - a) .* vals_Q1 + a .* vals_Q2;
+
+    % desfazer o hack
+    outP = reshape(out2D, [new_s(dim), s_mat_p(2:end)]);
+    out = ipermute(outP, order);
+    out = cast(out, 'like', mat);
 end
